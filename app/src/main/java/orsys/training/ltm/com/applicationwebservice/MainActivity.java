fac3a92e07704562549fed9d5156337f42642361
Bertrand Leclercq - Mac Book Pro <bl@ltm.fr>
2020-06-26 08:57:51 +0200

    migration to androidx

--- app/src/main/java/orsys/training/ltm/com/applicationwebservice/MainActivity.java
+++ app/src/main/java/orsys/training/ltm/com/applicationwebservice/MainActivity.java
@@ -1,19 +1,15 @@
 package orsys.training.ltm.com.applicationwebservice;
 
-import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
